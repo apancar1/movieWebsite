404d3b627d801ec80c124fc42a64fecb7a6d697f
Arsheen <pancar@usc.edu>
2025-06-05 16:07:21 -0700

    Fix API bug in App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieCard from './MovieCard';
 import './App.css';
 import SearchIcon from './search.svg';
 
-const API_URL = 'http://www.omdbapi.com?apikey=63def76f';
+const API_URL = 'https://www.omdbapi.com?apikey=63def76f';
 
 // const movie1 = {
 //     "Title": "Superman, Spiderman or Batman",
